20e4b593f2692a550ea03de7fc4167255e7a382b
leonardoCavazzani <cavazzanileonardo@gmail.com>
2017-10-25 19:17:25 -0200

    input

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,13 +1,55 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
-class ListConctact extends Component{
+class ListContact extends Component{
+    
+    state ={
+        query: ''
+    }
+    updateQuery = (query) => {
+        this.setState({
+            query: query.trim()
+        })
+        
+    }
     render(){
         return(
-            <ol className='contact-list'>
-                {this.props.contacts.map(contact=><li key={contact.id}>{contact.name}</li>)}
-            </ol>
+            <div className='list-contacts'>
+                <div className='list-contacts-top'>
+                    <input className='search-contacts' 
+                       type= 'text' 
+                       placeholder='Search...' 
+                       value={this.state.query} 
+                       onChange={(event)=> this.updateQuery(event.target.value)}>
+                        
+                    </input>    
+                </div>
+                 <ol className='contact-list'>
+                {this.props.contacts.map(contact=>
+                                         <li key={contact.id} className='contact-list-item'>
+                                             <div className='contact-avatar' style={{
+                                                     backgroundImage: `url(${contact.avatarURL})`
+                                                    }}/>
+                                             <div className='contact-details'>
+                                                 <p>{contact.name}</p>
+                                                 <p>{contact.email}</p>
+                                             </div>
+                                             <div>
+                                                 <button className='contact-remove' onClick={()=> this.props.remove(contact)}>
+                                                     botao
+                                                 </button>
+                                             </div>
+                                         </li>
+                                        )}
+                </ol>
+            </div>
+           
         )
     }
 }
 
-export default ListConctact
+    ListContact.PropTypes ={
+        contacts: PropTypes.array.isRequired,
+        remove: PropTypes.func.isRequired,
+    }
+export default ListContact
