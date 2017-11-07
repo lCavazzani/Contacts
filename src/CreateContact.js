d26e2bffc652c92b1d0cf5bbdfb02d8cbab8db77
lCavazzani <cavazzanileonardo@gmail.com>
2017-11-07 15:12:28 -0200

    serializable create contact

--- src/CreateContact.js
+++ src/CreateContact.js
@@ -1,11 +1,27 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import ImageInput from './ImageInput';
+import serializeForm from 'form-serialize'
 
-class CreateContact extends Component{
-    render(){
-        return(
-        <div> Hi </div>
-        )
-    }
+class CreateContact extends Component {
+  handleSubmit = (e) =>{
+    e.preventDefault();
+    const value = serializeForm(e.target,{hash:true})
+    this.props.onCreateContact(value);
+  }
+  render() {
+    return (<div>
+      <Link className="close-create-contact" to="/">Close</Link>
+      <form onSubmit={this.handleSubmit}className="create-contact-form">
+        <ImageInput className="create-contact-avatar-input" name="avatarURL" maxHeight={64} maxWidth={64}/>
+        <div className="create-contact-details">
+          <input type="text" name="name" placeholder="name"/>
+          <input type="text" name="email" placeholder="email"/>
+          <button>ADD CONTACT</button>
+        </div>
+      </form>
+    </div>)
+  }
 }
 
 export default CreateContact;
